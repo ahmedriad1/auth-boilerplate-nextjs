d1ebe159f824561f153e79f7be617fdc8fb632c3
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2022-01-15 14:14:14 +0300

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './helpers/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
